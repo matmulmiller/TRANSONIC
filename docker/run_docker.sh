0a86f9a2f4246e68731dfc577ddac79c1c20dac3
John White <johnwhlte@proton.me>
2024-09-07 18:40:18 -0500

    new test

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,4 +2,4 @@
 
 TRANSONIC_PATH=~/spades/TRANSONIC
 
-sudo docker container run -ti -v ${TRANSONIC_PATH}/user_data:/root/TRANSONIC/user_data/ transonic
+sudo docker container run -ti -v ${TRANSONIC_PATH}/user_data:/root/TRANSONIC/user_data/ -v "${HOME}/.Xauthority":"/root/.Xauthority:ro" -e "DISPLAY=$DISPLAY" --network host transonic
